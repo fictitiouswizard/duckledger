5a0370f5f57a7cf7ed262b67a4578eee3accdbcc
Jason Sanders <fictitiouswizard@gmail.com>
2023-12-22 19:05:39 -0600

    css shenanigans

--- bills/views.py
+++ bills/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
 # Create your views here.
+from bills.models import Bill, Transaction
+
 
 def list_bills(request):
-   return render(request, "bills/list_bills.html", {})
+   bills = Bill.objects.all()
+   transactions = Transaction.objects.all()
+   return render(
+      request,
+      "bills/bill_list.html",
+      {
+         "bills": bills,
+         "transactions": transactions
+      }
+   )
+
